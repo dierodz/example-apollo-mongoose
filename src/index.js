2e6c22f99633ea0e8e79ae9a2d482c412b9bd466
Diego Rodriguez <dierodz91@gmail.com>
2020-12-15 13:09:38 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import express from "express"
 import { ApolloServer, gql } from "apollo-server-express";
 import typeDefs from './typeDefs'
 import resolvers from './resolvers'
+const http = require("http");
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const app = express()
-
+const serverhttp = http.createServer(app);
 server.applyMiddleware({ app })
+server.installSubscriptionHandlers(serverhttp);
 
-app.listen(8080)
+app.listen(8080, () => {
+  console.log("ok");
+});
